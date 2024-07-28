da47cf97c3027a334972ec6ef1812b46f3b790b6
transang <tranquangsang12.7@gmail.com>
2024-07-28 18:08:55 +0700

    add git ingore

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log(`hello world`);
+#!/usr/bin/env node
+
+import { program } from "commander";
+
+// program.option("--first").option("-s, --separator <char>");
+
+// Adding action
+program.action(() => {
+  console.log(`hello`);
+});
+
+program
+  .command("generate <type>")
+  .description("Generate code for a given type")
+  .action(async (type: string) => {
+    console.log(type);
+  });
+// Execute program
+program.parse();
+
+// const options = program.opts();
+// const limit = options.first ? 1 : undefined;
+// console.log(program.args[0].split(options.separator, limit));
