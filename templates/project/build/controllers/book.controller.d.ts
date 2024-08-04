95eeaaa3d746ea377cb96b65235327d989f78a4b
Trần Quang Sáng <tranquangsang12.7@gmail.com>
2024-08-05 01:25:06 +0700

    Feature: Add on copy dir template

--- templates/project/build/controllers/book.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from '@sangtrandev/kompact';
-import { CreateBookDto, UpdateBookDto } from '../dto/book.dto';
-export declare class BookController {
-    getAllBook(_: Request, res: Response): void;
-    addBook(addBookDto: CreateBookDto, res: Response): void;
-    getBookById(bookId: string, res: Response): void;
-    updateBook(bookId: string, updateBookDto: UpdateBookDto, res: Response): void;
-    deleteBook(bookId: string, res: Response): void;
-}
