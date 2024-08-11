2f1adf79c1ba6260fa94287deb245abc8ed9169f
Trần Quang Sáng <tranquangsang12.7@gmail.com>
2024-08-11 22:15:28 +0700

    Feature: Add color for kompact

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -3,6 +3,8 @@ import path from 'path'
 import { execSync } from 'child_process'
 import { copyDirectory } from '../utils/copy'
 import { select } from '@inquirer/prompts'
+import figlet from 'figlet'
+import gradient from 'gradient-string'
 
 type PackageManager = 'npm' | 'yarn' | 'pnpm'
 
@@ -15,29 +17,40 @@ enum EPackageManager {
 export const command = new Command('create')
   .argument('<project-name>', 'Name of the project or use . for current folder')
   .description('Create a new Kompact project')
-  .action(projectName => {
-    // const packageType =
-    createProjectFromTemplate(projectName, 'npm')
-    // // Optionally, run npm install in the new project directory
-    // execSync('npm install', { cwd: projectPath, stdio: 'inherit' })
+  .action(async projectName => {
+    const packageType = await promptPackageManager()
+    createProjectFromTemplate(projectName, packageType)
+    figlet('Kompact', (err, data) => {
+      if (err) {
+        console.log('Something went wrong...')
+        console.dir(err)
+        return
+      }
+      console.log(gradient.teen(data))
+    })
   })
 
-async function promptPackageManager(): Promise<string> {
-  const answers = await select({
+async function promptPackageManager(): Promise<PackageManager> {
+  const answers = await select<PackageManager>({
     message: 'Which package manager would you like to use?',
     choices: [
       {
-        name: 'npm',
-        value: 'npm',
-        description: 'npm is the most popular package manager',
+        name: EPackageManager.Npm,
+        value: EPackageManager.Npm,
+        description: 'Most popular package manager',
       },
       {
-        name: 'yarn',
-        value: 'yarn',
-        description: 'yarn is an awesome package manager',
+        name: EPackageManager.Yarn,
+        value: EPackageManager.Yarn,
+        description: 'Awesome package manager',
+      },
+      {
+        name: EPackageManager.Pnpm,
+        value: EPackageManager.Pnpm,
+        description: 'Fast, disk space efficient package manager',
       },
     ],
-    // default: 'npm',
+    default: EPackageManager.Npm,
   })
 
   return answers
@@ -50,8 +63,22 @@ function createProjectFromTemplate(
   const currentPath = process.cwd()
   const projectPath =
     projectName === '.' ? currentPath : path.join(currentPath, projectName)
-  console.log(__dirname)
   const templateDir = path.join(__dirname, '../../templates/project')
-  copyDirectory(templateDir, projectPath)
-  console.log(`Project created at ${projectPath}`)
+  copyDirectory(templateDir, projectPath, projectName)
+  execSync(packageCommand[packageManager].install, {
+    cwd: projectPath,
+    stdio: 'inherit',
+  })
+}
+
+const packageCommand = {
+  npm: {
+    install: 'npm install',
+  },
+  yarn: {
+    install: 'yarn',
+  },
+  pnpm: {
+    install: 'pnpm install',
+  },
 }
--- src/utils/copy.ts
+++ src/utils/copy.ts
@@ -1,18 +1,29 @@
 import fs from 'fs'
 import path from 'path'
 
-export function copyDirectory(src: string, dest: string) {
+export function copyDirectory(src: string, dest: string, projectName: string) {
   fs.mkdirSync(dest, { recursive: true })
 
   const entries = fs.readdirSync(src, { withFileTypes: true })
-
   for (let entry of entries) {
     const srcPath = path.join(src, entry.name)
     const destPath = path.join(dest, entry.name)
-
+    console.log(entry.name)
     if (entry.isDirectory()) {
-      copyDirectory(srcPath, destPath)
+      copyDirectory(srcPath, destPath, projectName)
     } else {
+      // change the projectName
+      if (entry.name === 'package.json') {
+        const packageJsonPath = path.resolve(destPath)
+        console.log(`packageJsonPath:: `, packageJsonPath)
+        // parse json
+        const packageJson = JSON.parse(
+          fs.readFileSync(packageJsonPath, 'utf-8')
+        )
+        packageJson.name = projectName
+        console.log(packageJson)
+        fs.writeFileSync(dest, JSON.stringify(packageJson))
+      }
       fs.copyFileSync(srcPath, destPath)
     }
   }
